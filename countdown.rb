bc5d6fc8b8aca966bd55fd96b2f835d5d47d61cd
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-23 03:07:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,13 @@
 def countdown(time)
   while time>=0
-   if time>1
-    puts"#{time} SECONDS"
-    time-=1
-   elsif time==1 
-    puts"#{time}
-    time-=1 
+  
+   if time!=0
+     puts "#{time} SECONDS!"
+     time-=1 
+   else
+     puts "#{time} SECONDS!"
+     time-=1 
+   end 
+   
   end 
 end
